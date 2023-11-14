bdc8aee35aba7ca22f09b4b43df9e1449dca71de
Maggie McGreal <maggie.mcgreal@selu.edu>
2023-11-14 20:57:50 +0000

    update NAMESPACE

--- R/linear_model.R
+++ R/linear_model.R
@@ -1,5 +1,6 @@
 #This statistical function will run a linear model to explore how accelerometer or gyroscope data (independent variables) may be associated with or predict behaviors (the dependent variable).
 #The 3 independent variables represent the X, Y, and Z axes of either accelerometer or gyroscope data
+#________________________________________________________________________________________-
 
 runLinearModel <- function(data, dependent_var, independent_var1, independent_var2, independent_var3) {
   formula <- as.formula(paste(dependent_var, "~", independent_var1, "+", independent_var2, "+", independent_var3))
